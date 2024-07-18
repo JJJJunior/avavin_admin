c46f3b0cbc094ed86c02e594524196484f022382
Maxwell <xj.master@gmail.com>
2024-07-18 16:49:33 +0800

    修改全局查询

--- app/api/search/[query]/route.ts
+++ app/api/search/[query]/route.ts
@@ -1,7 +1,7 @@
-import {NextResponse, NextRequest} from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/prisma";
 
-export const GET = async (req: NextRequest, {params}: { params: { query: string } }) => {
+export const GET = async (req: NextRequest, { params }: { params: { query: string } }) => {
   try {
     const searchedProducts = await prisma.product.findMany({
       where: {
@@ -9,27 +9,23 @@ export const GET = async (req: NextRequest, {params}: { params: { query: string 
           {
             title: {
               contains: params.query,
-              mode: "insensitive"
-            }
+            },
           },
           {
             category: {
               contains: params.query,
-              mode: "insensitive"
-            }
+            },
           },
           {
-            tags: {
-              has: params.query,
-            }
-          }  //match an array of values
-        ]
-      }
-    })
+            tags: params.query,
+          },
+        ],
+      },
+    });
 
-    return NextResponse.json(searchedProducts, {status: 200});
+    return NextResponse.json(searchedProducts, { status: 200 });
   } catch (err) {
     console.log(err);
-    return new NextResponse("Internal server error!", {status: 500})
+    return new NextResponse("Internal server error!", { status: 500 });
   }
-}
+};
